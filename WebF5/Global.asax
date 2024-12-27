9dc36d8639dff1b706bbd868310f08bd2ff70523
Sagan18 <egallegogo@yahoo.es>
2024-12-27 15:34:45 +0100

    Agregar archivos de proyecto.

---
+++ WebF5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebF5.MvcApplication" Language="C#" %>
---
+++ WebF5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebF5.Startup))]
+namespace WebF5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
